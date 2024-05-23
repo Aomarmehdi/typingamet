3672574af3e9c22e4fbe4bb85c9b04d75d59dd7e
Aomarmehid <aouraghomarmehdi@gmail.com>
2024-05-23 16:07:22 +0100

    a new database

--- type.sql
+++ type.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : mer. 22 mai 2024 à 12:26
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
+-- Host: localhost
+-- Generation Time: May 23, 2024 at 05:05 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `type`
+-- Database: `type`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `joueur`
+-- Table structure for table `joueur`
 --
 
 CREATE TABLE `joueur` (
@@ -35,17 +35,18 @@ CREATE TABLE `joueur` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `joueur`
+-- Dumping data for table `joueur`
 --
 
 INSERT INTO `joueur` (`id_joueur`, `user_name`, `email`, `password`) VALUES
-(1, 'mehdi', 'mehdi@gmail.com', 'mehdi'),
-(2, 'nassim', 'nassim@gmail.com', 'nassim');
+(75, 'omar mehdi', 'omarmehdi@gmail.com', 'mehdi'),
+(76, 'hello ', 'hello@gmail.com', 'hello'),
+(77, 'mehdi', 'mehdi@gmail.com', 'mehdi');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `test`
+-- Table structure for table `test`
 --
 
 CREATE TABLE `test` (
@@ -55,49 +56,61 @@ CREATE TABLE `test` (
   `mistakes` varchar(100) NOT NULL,
   `wpm` varchar(100) NOT NULL,
   `cpm` varchar(100) NOT NULL,
-  `mode_punctuation` tinyint(1) NOT NULL,
-  `mode_numbers` tinyint(1) NOT NULL
+  `mode_punctuation` text NOT NULL,
+  `mode_numbers` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Index pour les tables déchargées
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id_test`, `id_joueur`, `time`, `mistakes`, `wpm`, `cpm`, `mode_punctuation`, `mode_numbers`) VALUES
+(1, 76, '15', '11', '40', '54', 'false', ' true'),
+(2, 76, '15', '0', '64', '58', 'false', ' true'),
+(3, 77, '15', '0', '60', '47', 'false', ' true'),
+(4, 77, '15', '2', '44', '55', 'false', ' false'),
+(6, 77, '15', '0', '64', '73', 'false', ' false'),
+(7, 77, '15', '0', '64', '58', 'true', ' true');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `joueur`
+-- Indexes for table `joueur`
 --
 ALTER TABLE `joueur`
   ADD PRIMARY KEY (`id_joueur`);
 
 --
--- Index pour la table `test`
+-- Indexes for table `test`
 --
 ALTER TABLE `test`
   ADD PRIMARY KEY (`id_test`),
   ADD KEY `id_joueur` (`id_joueur`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `joueur`
+-- AUTO_INCREMENT for table `joueur`
 --
 ALTER TABLE `joueur`
-  MODIFY `id_joueur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `id_joueur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
--- AUTO_INCREMENT pour la table `test`
+-- AUTO_INCREMENT for table `test`
 --
 ALTER TABLE `test`
-  MODIFY `id_test` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_test` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Contraintes pour les tables déchargées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `test`
+-- Constraints for table `test`
 --
 ALTER TABLE `test`
   ADD CONSTRAINT `test_ibfk_1` FOREIGN KEY (`id_joueur`) REFERENCES `joueur` (`id_joueur`);
